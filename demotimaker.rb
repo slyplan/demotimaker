aa5c9dfab403fda656c83bed973adf4d5f2cca43
Kalmykov Kirill <kkalmykov@at-consulting.ru>
2012-07-31 23:27:43 +0400

    Added generation title and text

--- demotimaker.rb
+++ demotimaker.rb
@@ -4,7 +4,7 @@ class Demotimaker
 
   include Magick
   
-  CONFIG = {:max_width => 500, :max_height => 400, :border => 40, :textarea => 100 }
+  CONFIG = {:max_width => 500, :max_height => 400, :border => 40, :textarea => 100, :title_pointsize => 64, :text_pointsize => 24}
 
   def initialize(image, title = "", text = "")
     @image = Image.read(image).first
@@ -12,6 +12,9 @@ class Demotimaker
     height_prop = CONFIG[:max_height].to_f / @image.rows.to_f
     coef = width_prop > height_prop ? width_prop : height_prop
     @image.resize!(coef)
+
+    @full_width = @image.columns + CONFIG[:border] * 2
+    @full_height = @image.rows + CONFIG[:border] * 2 + CONFIG[:textarea]
     
     @title = title
     @text  = text
@@ -27,8 +30,25 @@ class Demotimaker
   end
 
   def generate
-    background = Image.new(@image.columns + CONFIG[:border] * 2, @image.rows + CONFIG[:border] * 2 + CONFIG[:textarea]) { self.background_color = 'black'}
+    background = Image.new(@full_width, @full_height) { self.background_color = 'black'}
     composition = background.composite(@image, CONFIG[:border], CONFIG[:border], OverCompositeOp)
-    
+
+    title_draw = Draw.new
+    composition = composition.annotate(title_draw, 0, 0, @full_width / 2, @image.rows + CONFIG[:border] * 2 + CONFIG[:title_pointsize] / 2, @title) {
+      self.font = File.join(File.dirname('__FILE__'), 'fonts', 'times.ttf')
+      self.fill = 'white'
+      self.pointsize = CONFIG[:title_pointsize]
+      self.align = CenterAlign
+    }
+
+    composition = composition.annotate(title_draw, 0, 0, @full_width / 2, @image.rows + CONFIG[:border] * 2 + CONFIG[:title_pointsize] + CONFIG[:text_pointsize] / 2, @text) {
+      self.font = File.join(File.dirname('__FILE__'), 'fonts', 'times.ttf')
+      self.fill = 'white'
+      self.pointsize = CONFIG[:text_pointsize]
+      self.align = CenterAlign
+    }
+
+
+    @demotivator = composition
   end
 end
